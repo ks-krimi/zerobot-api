842dfa4061a79dde697396b6b61386245022238c
Ks Krimi <ny.kalash@gmail.com>
2022-10-26 17:37:48 +0200

    feat(router): crée un route pour obtenir un utilisateur

--- routers/user.py
+++ routers/user.py
@@ -26,3 +26,14 @@ def create_user(user: UserCreate, db: Session = Depends(get_db)):
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Email is already used"
         )
+
+
+@router.get('/{id}', response_model=User)
+def get_user(id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    user = db.query(model_user).filter(model_user.id == id).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"User with id {id} does not exist"
+        )
+    return user
