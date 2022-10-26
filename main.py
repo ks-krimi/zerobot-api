7c02bed91bd4f1f8696a8f3b70e0513bcaef31fd
Ks Krimi <ny.kalash@gmail.com>
2022-10-26 17:46:24 +0200

    conf(cors): configure le CORS

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from database.base import Base
 from database.session import engine
@@ -8,6 +9,15 @@ Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(auth.router)
