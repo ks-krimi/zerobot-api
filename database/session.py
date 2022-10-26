2d5ce4de6e27963f197a0d7661d8469d437cefc8
Ks Krimi <ny.kalash@gmail.com>
2022-10-26 17:14:08 +0200

    conf(bd): configure sqlalchemy

---
+++ database/session.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from config import settings
+
+SQLALCHEMY_DATABASE_URL = settings.sqlalchemy_database_url
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
